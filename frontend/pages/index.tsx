379e1c8eed133598b7ebacf9f592900e4efa39a6
Jinglei Ren <jinglei@merico.dev>
2023-06-04 20:46:54 +0800

    Create useSignIn hook and refactor Home component
    - Add new useSignIn hook to handle sign-in logic.
    - Remove sign-in logic from Home component and use useSignIn hook.
    - Update import statements in Home component.

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,33 +1,12 @@
-import apiClient from '../app/apiClient';
 import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
-import { isAxiosError } from 'axios';
 import SignInForm from '../components/SignInForm';
+import useSignIn from '../hooks/useSignIn';
 
 const Home: React.FC = () => {
   const [activeTab, setActiveTab] = useState('signin');
-  const [accessKey, setAccessKey] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const router = useRouter();
-
-  const handleSignIn = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      const response = await apiClient.post('/api/v1/login', { key: accessKey });
-      const { user_id } = response.data;
-      localStorage.setItem('user_id', user_id.toString());
-      router.push('/profile');
-    } catch (error: unknown) {
-      console.error('Error signing in:', error);
-      if (isAxiosError(error) && error.response && error.response.status === 401) {
-        setErrorMessage('Failed to sign in. Please check your access key.');
-      } else {
-        setErrorMessage('Network error. Please check your connection and try again.');
-      }
-    }
-  };
+  const { accessKey, setAccessKey, errorMessage, handleSignIn } = useSignIn();
 
   return (
     <div className="container mx-auto px-4">
