6e8da44330b8ee00caeb992effa0917b6f64dc05
Jinglei Ren <jinglei@merico.dev>
2023-06-05 13:27:55 +0800

    Refactor styles and update dark mode colors
    - Import pageStyles.css in index.tsx.
    - Move container and main styles to pageStyles.css.
    - Update dark mode colors in formStyles.css.
    - Adjust text color for active and inactive tabs in index.tsx.

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -5,6 +5,7 @@ import SignInForm from '../components/SignInForm';
 import SignUpForm from '../components/SignUpForm';
 import useSignIn from '../hooks/useSignIn';
 import useSignUp from '@/hooks/useSignUp';
+import '@/styles/pageStyles.css';
 
 const Home: React.FC = () => {
   const [activeTab, setActiveTab] = useState('signin');
@@ -12,27 +13,27 @@ const Home: React.FC = () => {
   const { signUpErrorMessage, signUpSuccessMessage, handleSignUp } = useSignUp();
 
   return (
-    <div className="container mx-auto px-4">
+    <div className="container">
       <Head>
         <title>Sign In & Sign Up</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center min-h-screen py-2">
+      <main className="main">
         <Image src="/devchat.png" alt="Logo" width={120} height={120} style={{ marginBottom: '2rem' }} />
         <h1 className="text-4xl font-bold mb-8">Welcome to DevChat!</h1>
 
-        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col w-[400px]">
+        <div className="card w-[400px]">
           <div className="flex mb-4">
             <button
-              className={`text-2xl font-semibold mr-4 ${activeTab === 'signin' ? 'text-blue-500' : 'text-gray-500'
+              className={`text-2xl font-semibold mr-4 ${activeTab === 'signin' ? 'text-blue-500' : 'text-gray-400'
                 }`}
               onClick={() => setActiveTab('signin')}
             >
               Sign In
             </button>
             <button
-              className={`text-2xl font-semibold ${activeTab === 'signup' ? 'text-blue-500' : 'text-gray-500'
+              className={`text-2xl font-semibold ${activeTab === 'signup' ? 'text-blue-500' : 'text-gray-400'
                 }`}
               onClick={() => setActiveTab('signup')}
             >
