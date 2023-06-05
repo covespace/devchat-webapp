ddbf8d866650d9c9bd8f6a1a09eaed38c933658c
Jinglei Ren <jinglei@merico.dev>
2023-06-05 13:02:38 +0800

    Create formStyles.css to refactor form components
    - Add formStyles.css with form-label, form-input & form-button classes.
    - Add focus and hover styles for dark mode.
    - Import formStyles.css in SignInForm.tsx and SignUpForm.tsx.
    - Apply form-bg-dark class to form elements for dark mode background.
    - Update .vscode/settings.json to set tabSize for CSS.

--- frontend/components/SignUpForm.tsx
+++ frontend/components/SignUpForm.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '@/styles/formStyles.css';
 
 interface SignUpFormProps {
   onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
@@ -6,37 +7,41 @@ interface SignUpFormProps {
   signUpSuccessMessage: string;
 }
 
-const SignUpForm: React.FC<SignUpFormProps> = ({ onSubmit, signUpErrorMessage, signUpSuccessMessage }) => {
+const SignUpForm: React.FC<SignUpFormProps> = ({
+  onSubmit,
+  signUpErrorMessage,
+  signUpSuccessMessage,
+}) => {
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={onSubmit} className="form-bg-dark">
       <div className="mb-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+        <label className="form-label" htmlFor="username">
           Username
         </label>
         <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          className="form-input"
           id="username"
           type="text"
           placeholder="3 to 39 alphanumeric chars or '-'"
         />
       </div>
       <div className="mb-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+        <label className="form-label" htmlFor="email">
           Email
         </label>
         <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          className="form-input"
           id="email"
           type="email"
           placeholder="Enter your email"
         />
       </div>
       <div className="mb-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="org-name">
+        <label className="form-label" htmlFor="org-name">
           Create Organization (Optional)
         </label>
         <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          className="form-input"
           id="org-name"
           type="text"
           placeholder="Enter your organization name"
@@ -45,13 +50,10 @@ const SignUpForm: React.FC<SignUpFormProps> = ({ onSubmit, signUpErrorMessage, s
       {signUpErrorMessage && (
         <p className="text-red-500 text-xs italic mt-2">{signUpErrorMessage}</p>
       )}
-      {signUpSuccessMessage && ( // Add this block
+      {signUpSuccessMessage && (
         <p className="text-green-500 text-xs italic mt-2">{signUpSuccessMessage}</p>
       )}
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        type="submit"
-      >
+      <button className="form-button" type="submit">
         Sign Up
       </button>
     </form>
