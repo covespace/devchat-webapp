c9f91e774d99be88c6429f78e2e2333ca2e4f728
Jinglei Ren <jinglei@merico.dev>
2023-06-08 06:47:23 +0800

    Remove organizations endpoints

--- webapp/api/v1/entities/routers.py
+++ webapp/api/v1/entities/routers.py
@@ -1,9 +1,6 @@
 from fastapi import APIRouter
-
-from .organizations import router as organizations_router
 from .users import router as users_router
 
 router = APIRouter()
 
-router.include_router(organizations_router)
 router.include_router(users_router)
